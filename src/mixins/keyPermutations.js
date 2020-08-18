74de88cfc600afa8bb14b29bb372301838670f48
Jan Bican <bican.honza@gmail.com>
2020-08-18 11:59:35 +0200

    KeyPermutationsRepeated update

--- src/mixins/keyPermutations.js
+++ src/mixins/keyPermutations.js
@@ -6,27 +6,31 @@ export default {
       keyIndices.sort(function(x, y) {
         return key.charCodeAt(x) - key.charCodeAt(y)
       })
+
       let keyPermutation = new Array(key.length)
       for (let i = 0; i < key.length; i++) {
         keyPermutation[keyIndices[i]] = i
       }
+
       return keyPermutation
     },
-    getKeyPermutationDuplicatesArray(key) {
-      let sortedKeyCharArray = [...key].sort()
-      let charMap = new Map()
-      for (let i = 0, j = 0; i < key.length; i++) {
-        const char = sortedKeyCharArray[i]
-        if (!charMap.has(char)) {
-          charMap.set(char, j)
-          j += 1
+    getKeyPermutationRepeated(key) {
+      const sortedLetters = [...key].sort()
+      const letterMap = new Map()
+      let position = 0
+
+      for (const letter of sortedLetters) {
+        if (!letterMap.has(letter)) {
+          letterMap.set(letter, position++)
         }
       }
-      let keyPermutationArray = new Array(key.length)
+
+      const keyPermutation = new Array(key.length)
       for (let i = 0; i < key.length; i++) {
-        keyPermutationArray[i] = charMap.get(key.charAt(i))
+        keyPermutation[i] = letterMap.get(key.charAt(i))
       }
-      return keyPermutationArray
+
+      return keyPermutation
     }
   }
 }
