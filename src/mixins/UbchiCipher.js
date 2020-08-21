b3eedbf2e85b988003f15541c261798773c02224
Jan Bican <bican.honza@gmail.com>
2020-08-21 11:03:24 +0200

    UbchiCipher prototype

---
+++ src/mixins/UbchiCipher.js
@@ -0,0 +1,34 @@
+import ColumnarCipher from '@/mixins/ColumnarCipher'
+
+export default {
+  name: 'UbchiCipher',
+  mixins: [ColumnarCipher],
+  methods: {
+    ubchiEncrypt(keyPermutation, numOfWords, text) {
+      // první transpozice
+      let partCipher = this.columnarEncrypt(keyPermutation, text)
+
+      // přidání "klamačů"
+      const deceiver = 'z'.repeat(numOfWords)
+      partCipher += deceiver
+
+      // druhá transpozice
+      const cipher = this.columnarEncrypt(keyPermutation, partCipher)
+
+      return cipher.toUpperCase()
+    },
+
+    ubchiDecrypt(keyPermutation, numOfWords, cipher) {
+      // první transpozice
+      let partPlain = this.columnarDecrypt(keyPermutation, cipher)
+
+      // odstranění "klamačů"
+      partPlain = partPlain.slice(0, -numOfWords)
+
+      // druhá transpozice
+      const plainText = this.columnarDecrypt(keyPermutation, partPlain)
+
+      return plainText.toLowerCase()
+    }
+  }
+}
