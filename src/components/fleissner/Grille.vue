742c5dedd17f1f1aa800c71a2e5214177114c24b
Jan Bican <bican.honza@gmail.com>
2020-08-29 14:29:32 +0200

    Grille Refactor

--- src/components/fleissner/Grille.vue
+++ src/components/fleissner/Grille.vue
@@ -1,40 +1,32 @@
 <template>
   <div class="grille">
-    <b-row class="mb-3">
-      <b-col>
-        <size-input
-          v-model.number="size"
-          :value="size"
-          label="Velikost mřížky"
-          invalidFeedback="Velikost mřížky musí být alespoň 4"
-          :isValid="isSizeValid"
-          @valueChanged="sizeChanged"
-        />
-      </b-col>
-    </b-row>
+    <size-input
+      v-model.number="size"
+      :value="size"
+      label="Velikost mřížky"
+      invalidFeedback="Velikost mřížky musí být alespoň 4"
+      :isValid="isSizeValid"
+      @valueChanged="sizeChanged"
+    />
 
-    <div v-show="isSizeValid">
-      <b-row class="justify-content-md-center mt-3 text-center">
-        <b-col>
-          <label>Mřížka</label>
-          <div class="grille-grid">
-            <table>
-              <tbody>
-                <tr v-for="(row, rowIndex) in grille" :key="rowIndex">
-                  <grille-cell
-                    v-for="(cell, cellIndex) in row"
-                    :key="cellIndex"
-                    :grille="grille"
-                    :row="rowIndex"
-                    :col="cellIndex"
-                    @click="cellClicked"
-                  ></grille-cell>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </b-col>
-      </b-row>
+    <div v-show="isSizeValid" class="mt-5">
+      <label>Mřížka</label>
+      <div class="grille-grid">
+        <table>
+          <tbody>
+            <tr v-for="(row, rowIndex) in grille" :key="rowIndex">
+              <grille-cell
+                v-for="(cell, cellIndex) in row"
+                :key="cellIndex"
+                :grille="grille"
+                :row="rowIndex"
+                :col="cellIndex"
+                @click="cellClicked"
+              ></grille-cell>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -51,25 +43,18 @@ export default {
   },
   data() {
     return {
-      size: 4,
-      grille: [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-      ],
-      count: 0
+      size: 5,
+      count: 0,
+      grille: null
     }
+  },
+  created: function() {
+    this.grille = this.createFreshGrille()
   },
   methods: {
     sizeChanged() {
       this.grille = this.createFreshGrille()
       this.count = 0
-
-      if (this.size % 2 == 1) {
-        const middle = Math.floor(this.size / 2)
-        this.grille[middle][middle] = 2
-      }
 
       this.$emit('change', null)
     },
@@ -79,16 +64,22 @@ export default {
       for (let i = 0; i < this.size; i++) {
         grille.push(new Array(this.size).fill(0))
       }
+
+      if (this.size % 2 == 1) {
+        const middle = Math.floor(this.size / 2)
+        grille[middle][middle] = 2
+      }
+
       return grille
     },
 
     cellClicked(row, col) {
       const value = this.grille[row][col]
-      if (value == 0) {
-        this.cutOut(row, col)
+      if (value === 0) {
+        this.cutCellOut(row, col)
         this.count += 1
       } else if (value == 1) {
-        this.fill(row, col)
+        this.fillCell(row, col)
         this.count -= 1
       }
 
@@ -96,26 +87,28 @@ export default {
     },
 
     isComplete() {
-      console.log(this.size)
-      console.log(this.count)
       return Math.floor((this.size * this.size) / 4) === this.count
     },
 
-    cutOut(row, col) {
+    cutCellOut(row, col) {
       this.grille[row][col] = 1
       this.grille[col][this.size - 1 - row] = 2
       this.grille[this.size - 1 - row][this.size - 1 - col] = 2
       this.grille[this.size - 1 - col][row] = 2
 
+      // pro spustění automatické detekce změny
+      // z důvodu překreslení mřížky
       this.$set(this.grille[row], col, 1)
     },
 
-    fill(row, col) {
+    fillCell(row, col) {
       this.grille[row][col] = 0
       this.grille[col][this.size - 1 - row] = 0
       this.grille[this.size - 1 - row][this.size - 1 - col] = 0
       this.grille[this.size - 1 - col][row] = 0
 
+      // pro spustění automatické detekce změny
+      // z důvodu překreslení mřížky
       this.$set(this.grille[row], col, 0)
     }
   },
@@ -130,9 +123,5 @@ export default {
 <style scoped>
 table {
   margin: 0 auto;
-}
-
-.grille-grid {
-  overflow-x: auto;
 }
 </style>
